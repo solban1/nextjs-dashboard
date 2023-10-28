8f5e54bd71568cdf163e6edb13297c1cfe8ea94b
Sol-ban Bae <solban1@daum.net>
2023-10-29 02:50:42 +0900

    Progress chapter 12

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,30 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+
+export default async function Page({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const invoice = await fetchInvoiceById(id);
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: "Invoices",
+            href: "/dashboard/invoices",
+          },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+          },
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice!} customers={customers} />
+    </main>
+  );
+}
